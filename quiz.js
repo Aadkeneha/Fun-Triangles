42c730b450347ef076f7cb0393d5b675bf7ec241
Neha Aadke <aadkeneha@gmail.com>
2021-09-28 19:14:00 +0530

    updated angles

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const formQuiz = document.querySelector(".form-quiz");
 const btnSubmit = document.querySelector("#btn-submit");
 const outputelement = document.querySelector("#output");
 
-const ans = ["90","right"];
+const ans = ["90°","right"];
 
 
 function checkans(){
